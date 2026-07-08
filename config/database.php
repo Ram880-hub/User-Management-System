01e65a00cd590c6370ba336136957329ac28b648
Ram880-hub <adapasreeraman@gmail.com>
2026-07-08 22:56:37 +0530

    Add database connection configuration

---
+++ config/database.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "user_management";
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if($conn->connect_error){
+    die("Connection Failed: " . $conn->connect_error);
+}
+
+?>
